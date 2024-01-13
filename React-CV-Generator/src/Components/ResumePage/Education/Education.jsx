f5ac40305ccf057829dc5a0d889f0e4590565303
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-01-12 19:14:02 -0500

    further styling education section

--- React-CV-Generator/src/Components/ResumePage/Education/Education.jsx
+++ React-CV-Generator/src/Components/ResumePage/Education/Education.jsx
@@ -3,13 +3,13 @@ import "../Education/Education.css";
 const Education = () => {
   return (
     <div className="educationSection">
-      <h2>Eduction</h2>
+      <h2 className="educationTitle">Education</h2>
       <div className="educationInfo">
         <div className="dateAndPlace">
           <p className="schoolDate">01/21 - 01/22</p>
           <p className="schoolLocation">Boston, MA</p>
         </div>
-        <div>
+        <div className="schoolInfo">
           <p className="schoolName">Harvard University</p>
           <p className="degreeName">Computer Science</p>
         </div>
