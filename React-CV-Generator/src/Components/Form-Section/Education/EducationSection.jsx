c7f283d7d6397a6c136a78e39bfc46c3dc2a9e25
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-02-07 21:51:21 -0500

    add fields to dynamic education form, and add feature to dynamically open and close the menu.

--- React-CV-Generator/src/Components/Form-Section/Education/EducationSection.jsx
+++ React-CV-Generator/src/Components/Form-Section/Education/EducationSection.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
+
 import FormInput from "../Personal-Info/Form-Input/FormInput";
 import "../Personal-Info/boxInfo.css";
 
 const EducationSection = () => {
+  const [toggleEdMenu, setToggleEdMenu] = useState(true);
+
+  function handleClick() {
+    if (toggleEdMenu) {
+      setToggleEdMenu(false);
+      console.log(toggle);
+    } else {
+      setToggleEdMenu(true);
+      console.log(toggle);
+    }
+  }
+
   return (
     <div className="boxInfoContainertwo">
-      <h2 className="boxHeading">Education</h2>
-      <form className="boxInfoForm">
-        <FormInput fullNameTitle="School" />
-        <FormInput fullNameTitle="Degree" />
-        <FormInput fullNameTitle="Start Date" />
-        <FormInput fullNameTitle="End Date" />
-        <FormInput fullNameTitle="Location" />
-      </form>
+      <div className="edHeadingContainer">
+        <h2 className="boxHeading">Education</h2>
+        <p onClick={handleClick} className="menuToggle">
+          {toggleEdMenu ? "+" : "-"}
+        </p>
+      </div>
+
+      {!toggleEdMenu ? (
+        <form className="boxInfoForm">
+          <FormInput fullNameTitle="School" />
+          <FormInput fullNameTitle="Degree" />
+          <FormInput fullNameTitle="Start Date" />
+          <FormInput fullNameTitle="End Date" />
+          <FormInput fullNameTitle="Location" />
+        </form>
+      ) : null}
     </div>
   );
 };
--- React-CV-Generator/src/Components/ResumePage/Experience/EdExperience.jsx
+++ React-CV-Generator/src/Components/ResumePage/Experience/EdExperience.jsx
@@ -1,10 +1,15 @@
+import "../Experience/EdExperience.css";
+
 const EdExperience = (props) => {
   return (
     <div>
       <div className="educationInfo">
         <div className="dateAndPlace">
-          <p className="schoolDate">{props.startDate}</p>
-          <p className="schoolDate">{props.endDate}</p>
+          <div className="dateContainer">
+            <p className="schoolDate"> {props.startDate}</p>
+            <p className="schoolDate">{props.endDate}</p>
+          </div>
+
           <p className="schoolLocation">{props.schoolLocation}</p>
         </div>
         <div className="schoolInfo">
