6f2115a38e02a7fde7427eb613b4da359509c9c3
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-02-08 19:25:00 -0500

    make personalize input box dynamic by adding function to open and close by pressing the plus and minus buttons

--- React-CV-Generator/src/Components/Form-Section/Personal-Info/PersonalInfo.jsx
+++ React-CV-Generator/src/Components/Form-Section/Personal-Info/PersonalInfo.jsx
@@ -1,19 +1,45 @@
 import FormInput from "./Form-Input/FormInput";
 import "../Personal-Info/boxInfo.css";
+import { useState } from "react";
 
 const PersonalInfo = (props) => {
+  const [toggleEdMenu, setToggleEdMenu] = useState(true);
+
+  function handleClick() {
+    if (toggleEdMenu) {
+      setToggleEdMenu(false);
+    } else {
+      setToggleEdMenu(true);
+    }
+  }
+
   return (
-    <div className="boxInfoContainer">
-      <h2 className="boxHeading">Personal Information</h2>
-      <form className="boxInfoForm">
-        <FormInput fullNameTitle="Full Name" onChange={props.onNameChange} />
-        <FormInput fullNameTitle="Email" onChange={props.onEmailChange} />
-        <FormInput
-          fullNameTitle="Phone Number"
-          onChange={props.onPhoneChange}
-        />
-        <FormInput fullNameTitle="Address" onChange={props.onLocationChange} />
-      </form>
+    <div
+      className={
+        !toggleEdMenu ? "boxInfoContainertwo-closed" : "boxInfoContainer"
+      }
+    >
+      <div className="edHeadingContainer">
+        <h2 className="boxHeading">Personal Information</h2>
+        <p onClick={handleClick} className="menuToggle">
+          {toggleEdMenu ? "-" : "+"}
+        </p>
+      </div>
+
+      {toggleEdMenu ? (
+        <form className="boxInfoForm">
+          <FormInput fullNameTitle="Full Name" onChange={props.onNameChange} />
+          <FormInput fullNameTitle="Email" onChange={props.onEmailChange} />
+          <FormInput
+            fullNameTitle="Phone Number"
+            onChange={props.onPhoneChange}
+          />
+          <FormInput
+            fullNameTitle="Address"
+            onChange={props.onLocationChange}
+          />
+        </form>
+      ) : null}
     </div>
   );
 };
