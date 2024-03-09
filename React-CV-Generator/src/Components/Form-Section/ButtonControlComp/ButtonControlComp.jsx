2cf94a77751bcc0cb07f952c896fbe65699f6359
Thiago de Matos <thiago.dematos.1@gmail.com>
2024-03-09 13:02:02 -0500

    create buttons to add education and work experience section and added style to buttons

--- React-CV-Generator/src/Components/Form-Section/ButtonControlComp/ButtonControlComp.jsx
+++ React-CV-Generator/src/Components/Form-Section/ButtonControlComp/ButtonControlComp.jsx
@@ -3,7 +3,9 @@ import "../Personal-Info/boxInfo.css";
 const ButtonControlComp = () => {
   return (
     <div className="control-buttons">
-      <button className="add-job-button">+ Experience</button>
+      <button className="add-job-button">+ Work</button>
+      <button className="add-education-button">+ Education</button>
+      <button className="reset-button">Reset</button>
     </div>
   );
 };
